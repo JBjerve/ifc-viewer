224907be39e7a8e7abb369a3fac806cd4401fbae
Markus Weberg <markus.weberg@gmail.com>
2023-08-19 21:04:09 +0200

    Fixed drop zone and resizing of the canvas

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const dragDropArea = document.getElementById('dragDropArea');
 const openFileExplorer = document.getElementById('openFileExplorer');
 const viewerContainer = document.getElementById('viewer-container');
+const canvas = document.getElementsByTagName('canvas');
 const fileBox = document.getElementById('fileBox');
 const banner = document.getElementById('banner');
 const pad = document.getElementById('pad');
@@ -20,6 +21,11 @@ dragDropArea.addEventListener('drop', (e) => {
     e.preventDefault();
     dragDropArea.classList.remove('highlight');
     const files = e.dataTransfer.files;
+
+    fileInput.files = files;
+    
+    const changeEvent = new Event('change', { bubbles: true });
+    fileInput.dispatchEvent(changeEvent);
 });
 
 openFileExplorer.addEventListener('click', () => {
@@ -36,6 +42,8 @@ fileInput.addEventListener('change', (e) => {
     if (files.length > 0) {
         fileBox.style.display = 'none';
         banner.style.display = 'none';
-        pad.style.display = 'none';     
+        pad.style.display = 'none';
+        viewerContainer.style.height = 100 + 'vh'
+        viewerContainer.style.widows = 100 + 'vw'
     }
 })
--- viewer.js
+++ viewer.js
@@ -4,26 +4,6 @@ import { IfcViewerAPI } from "web-ifc-viewer";
 const container = document.getElementById("viewer-container");
 const input = document.getElementById("fileInput");
 
-const viewer = new IfcViewerAPI({
-  container,
-  backgroundColor: new Color(0xffffff),
-});
-
-viewer.axes.setAxes();
-viewer.grid.setGrid();
-
-window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
-window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
-viewer.clipper.active = true;
-
-window.onkeydown = (event) => {
-  if (event.code === "KeyP") {
-    viewer.clipper.createPlane();
-  } else if (event.code === "KeyO") {
-    viewer.clipper.deletePlane();
-  }
-};
-
 input.addEventListener(
     "change",
   
@@ -31,9 +11,27 @@ input.addEventListener(
       const file = changed.target.files[0];
       const ifcURL = URL.createObjectURL(file);
 
+      const viewer = new IfcViewerAPI({
+        container,
+        backgroundColor: new Color(0xffffff),
+      });
+      
+      viewer.axes.setAxes();
+      viewer.grid.setGrid();
+      
+      window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
+      window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+      viewer.clipper.active = true;
+      
+      window.onkeydown = (event) => {
+        if (event.code === "KeyP") {
+          viewer.clipper.createPlane();
+        } else if (event.code === "KeyO") {
+          viewer.clipper.deletePlane();
+        }
+      };
 
-
-      viewer.IFC.loadIfcUrl(ifcURL);  
+      viewer.IFC.loadIfcUrl(ifcURL); 
     },
   
     false
